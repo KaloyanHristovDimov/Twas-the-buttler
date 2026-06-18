d44c5d5402cd87f102d6bebb6307463cea17c769
BoteRene <b33rbee@gmail.com>
2026-06-18 12:52:28 +0200

    eddited script name to make how its used clearer

--- Twas the butler/Assets/UI test scene assets/Scripts/TurnObjectScript.cs
+++ Twas the butler/Assets/UI test scene assets/Scripts/TurnObjectScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnCameraScript : MonoBehaviour
+public class TurnObjectScript : MonoBehaviour
 {
 
     [SerializeField] private GameObject rotatingObject;
