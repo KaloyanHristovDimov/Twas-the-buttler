d855552b44d50ac2df79bb238984067b1c879fd3
BoteRene <b33rbee@gmail.com>
2026-06-18 12:47:53 +0200

    removed the addidtion to the inputsystem, and eddited the zoomin code so that it might work with touchscreen

--- Twas the butler/Assets/UI test scene assets/Scripts/ClickLocationDetecter.cs
+++ Twas the butler/Assets/UI test scene assets/Scripts/ClickLocationDetecter.cs
@@ -1,4 +1,6 @@
 
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -9,30 +11,53 @@ public class ClickLocationDetector : MonoBehaviour
     [SerializeField] private Vector3 clickPosition;
     
     [SerializeField] private UnityEvent<Vector3> OnClick;
-    
-    
+
+    void Start()
+    {
+        Debug.Log("started");
+        Debug.Log(Pointer.current);
+        Debug.Log(Touchscreen.current);
+        Debug.Log("started fully");
+    }
+
     void Update() { 
         // Get the mouse click position in world space 
-        if (Mouse.current.leftButton.isPressed)
+        if (Pointer.current.press.isPressed)
         {
-            Debug.Log(Mouse.current.position.ReadValue()); 
-            Ray mouseRay = Camera.main.ScreenPointToRay( Mouse.current.position.ReadValue() ); 
-
-            if (Physics.Raycast( mouseRay, out RaycastHit hitInfo )) { 
-                Vector3 clickWorldPosition = hitInfo.point; 
-                Debug.Log(hitInfo.point);
-                
-                clickPosition = clickWorldPosition;
-                
-                Vector3 direction = mouseRay.direction;
-                Debug.DrawRay(mouseRay.origin, hitInfo.point - mouseRay.origin, Color.red, 5f);
-
-                //activate trigger and send it the coordinates of the click
-                
-                OnClick.Invoke(clickPosition);
-            }
+            CastRayForDetection(Pointer.current.position.ReadValue());
+            // Debug.Log(Pointer.current.position.ReadValue());
         }
+        
+        else if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            // Debug.Log("touchPosition:");
+            // Debug.Log(Touchscreen.current.primaryTouch.position.ReadValue());
+            CastRayForDetection(Touchscreen.current.primaryTouch.position.ReadValue());
+        }
+        
         
 
     }
+
+    private void CastRayForDetection(Vector2 screenPosition)
+    {
+        // Debug.Log(screenPosition);
+        Ray mouseRay = Camera.main.ScreenPointToRay(screenPosition);
+
+        if (Physics.Raycast(mouseRay, out RaycastHit hitInfo))
+        {
+            Vector3 clickWorldPosition = hitInfo.point;
+            // Debug.Log(hitInfo.point);
+
+            clickPosition = clickWorldPosition;
+
+            Vector3 direction = mouseRay.direction;
+            Debug.DrawRay(mouseRay.origin, hitInfo.point - mouseRay.origin, Color.red, 5f);
+
+            //activate trigger and send it the coordinates of the click
+
+            OnClick.Invoke(clickPosition);
+        }
+    }
+
 }
