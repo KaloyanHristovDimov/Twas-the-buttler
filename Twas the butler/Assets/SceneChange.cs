44442e497a25140ce62af7794a0befa4d203c610
Zebongus <il76vasylenko@gmail.com>
2026-06-19 14:45:30 +0200

    Added buttons for testing

---
+++ Twas the butler/Assets/SceneChange.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
