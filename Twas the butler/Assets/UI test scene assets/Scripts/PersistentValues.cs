73d211ebb8372cfb564840b2efed1d34d6e5faea
BoteRene <b33rbee@gmail.com>
2026-06-26 17:56:54 +0200

    made persistent audio sliders

--- Twas the butler/Assets/UI test scene assets/Scripts/PersistentValues.cs
+++ Twas the butler/Assets/UI test scene assets/Scripts/PersistentValues.cs
@@ -53,31 +53,4 @@ public class PersistentValues : MonoBehaviour
     public float bgm = 1f;
     public float sfx = 1f;
 
-    public UnityEvent<float> OnMasterChanged = new UnityEvent<float>();
-    public UnityEvent<float> OnBgmChanged = new UnityEvent<float>();
-    public UnityEvent<float> OnSfxChanged = new UnityEvent<float>();
-
-    public void SetMaster(float v)
-    {
-        v = Mathf.Clamp01(v);
-        if (Mathf.Approximately(master, v)) return;
-        master = v;
-        OnMasterChanged.Invoke(master);
-    }
-
-    public void SetBgm(float v)
-    {
-        v = Mathf.Clamp01(v);
-        if (Mathf.Approximately(bgm, v)) return;
-        bgm = v;
-        OnBgmChanged.Invoke(bgm);
-    }
-
-    public void SetSfx(float v)
-    {
-        v = Mathf.Clamp01(v);
-        if (Mathf.Approximately(sfx, v)) return;
-        sfx = v;
-        OnSfxChanged.Invoke(sfx);
-    }
 }
