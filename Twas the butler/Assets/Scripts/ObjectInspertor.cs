5dab6429a894145d1e1eddde32cbd08f83615692
KaloyanHristovDimov <KaloyanHristovDimov@gmail.com>
2026-06-29 18:49:21 +0200

    Add optional clone inspecting instead of just move

--- Twas the butler/Assets/Scripts/ObjectInspertor.cs
+++ Twas the butler/Assets/Scripts/ObjectInspertor.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class ObjectInspector : MonoBehaviour, IDragHandler 
+public class ObjectInspector : MonoBehaviour, IDragHandler
 {
     public static ObjectInspector Instance { get; private set; }
+    [SerializeField]
+    private bool copyObject = false;
 
     private GameObject selectedObject;
+    private GameObject copyOfSelectedObject;
 
     public float rotationSpeed = 1f;
     private Vector3 originalObjectPosition;
@@ -29,13 +32,21 @@ public class ObjectInspector : MonoBehaviour, IDragHandler
     public void StartInspection(GameObject gameObject, float inspectDistance)
     {
         if (selectedObject != null) return;
-        selectedObject = gameObject;
+        if (copyObject)
+        {
+            copyOfSelectedObject = Instantiate(gameObject);
+            copyOfSelectedObject.name = "Copy of inspected object";
+            selectedObject = copyOfSelectedObject;
+        }
+        else selectedObject = gameObject;
         originalObjectPosition = selectedObject.transform.position;
         originalObjectRotation = selectedObject.transform.rotation;
         selectedObject.transform.SetParent(transform);
         Vector3 objectScale = selectedObject.transform.localScale;
-        selectedObject.transform.localScale = new Vector3(objectScale.x/2, objectScale.y / 2, objectScale.z / 2);
-        selectedObject.transform.localPosition = new Vector3(inspectDistance/2, 0f, 0f);
+        if (copyObject) selectedObject.transform.localScale = new Vector3(objectScale.x * 50, objectScale.y * 50, objectScale.z * 50);
+        else selectedObject.transform.localScale = new Vector3(objectScale.x / 2, objectScale.y / 2, objectScale.z / 2);
+        selectedObject.transform.localPosition = new Vector3(inspectDistance / 2, 0f, 0f);
+
         onStartInspection.Invoke();
     }
 
@@ -59,6 +70,16 @@ public class ObjectInspector : MonoBehaviour, IDragHandler
     public void StopInspection()
     {
         if (selectedObject == null) return;
+        Debug.Log("Stopping inspection");
+        if (copyObject)
+        {
+            Debug.Log("copy object");
+            copyOfSelectedObject = GameObject.Find("Copy of inspected object");
+            Destroy(copyOfSelectedObject);
+            Debug.Log("Destroyed copy object");
+            return;
+        }
+        Debug.Log("Skipped copy object");
         selectedObject.transform.SetParent(null);
         Vector3 objectScale = selectedObject.transform.localScale;
         selectedObject.transform.localScale = new Vector3(objectScale.x * 2, objectScale.y * 2, objectScale.z * 2);
