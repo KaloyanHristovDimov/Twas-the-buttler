fd35319cf4bb1c00dfff66345abaed5ff8be1349
Zebongus <il76vasylenko@gmail.com>
2026-06-29 14:10:28 +0200

    Added collider requirement and relocated some scripts

--- Twas the butler/Assets/Scripts/Classes/InspectableObject.cs
+++ Twas the butler/Assets/Scripts/Classes/InspectableObject.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Collider))]
 public class InspectableObject : MonoBehaviour, IPointerClickHandler
 {
+   
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log($"Clicked on {gameObject.name}");
