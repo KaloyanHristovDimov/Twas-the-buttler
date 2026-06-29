5bfbdcd7b22fb1538f65a4a602561af6cab66394
BoteRene <b33rbee@gmail.com>
2026-06-29 15:35:39 +0200

    Changed inspectableObjec script to add disclaimer, and added InspectableObject script to clickable assets in scene Level2

--- Twas the butler/Assets/Scripts/Classes/InspectableObject.cs
+++ Twas the butler/Assets/Scripts/Classes/InspectableObject.cs
@@ -4,7 +4,10 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(Collider))]
 public class InspectableObject : MonoBehaviour, IPointerClickHandler
 {
-   
+
+    [SerializeField] private string Requirement = "This gameobject needs to be on the water layer to work";
+
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log($"Clicked on {gameObject.name}");
