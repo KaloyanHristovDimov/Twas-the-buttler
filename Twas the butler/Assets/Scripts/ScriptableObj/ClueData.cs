6ff6d939882ce3be0afcb366c7d57e51d98e3bba
BoteRene <b33rbee@gmail.com>
2026-07-01 12:21:30 +0200

    Working Sollution
    which sends you to main menu on correct completion

--- Twas the butler/Assets/Scripts/ScriptableObj/ClueData.cs
+++ Twas the butler/Assets/Scripts/ScriptableObj/ClueData.cs
@@ -7,6 +7,6 @@ public class ClueData : ScriptableObject
     public enum ClueType { Note, Object, Location, Person }
     public ClueType clueType;
     public Sprite clueSprite;
-    public enum StringTag { None, Used, Killed, Family, Wore, Recieved, Gave, About }
+    public enum StringTag { None, Used, Killed, Family, Wore, Recieved, Harrased, Gave, About }
     public StringTag asosiatedStringTag;
 }
