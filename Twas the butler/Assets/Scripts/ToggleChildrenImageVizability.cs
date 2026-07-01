693a43ecf1014956c0c707430a8331fb5cfc7a16
KaloyanHristovDimov <KaloyanHristovDimov@gmail.com>
2026-07-01 17:24:28 +0200

    Fix tag visibility

--- Twas the butler/Assets/Scripts/ToggleChildrenImageVizability.cs
+++ Twas the butler/Assets/Scripts/ToggleChildrenImageVizability.cs
@@ -17,11 +17,25 @@ public class ToggleChildrenImageVizability : MonoBehaviour
             if (child.GetComponent<Image>() != null) child.GetComponent<Image>().enabled = true;
             if (child.GetComponentInChildren<TextMeshProUGUI>() != null) child.GetComponentInChildren<TextMeshProUGUI>().enabled = true;
         }
+
+        foreach (TagSlot slot in InventoryManager.Instance.tagSlots)
+        {
+            Transform child = slot.transform;
+            if (child.GetComponent<Image>() != null) child.GetComponent<Image>().enabled = true;
+            if (child.GetComponentInChildren<TextMeshProUGUI>() != null) child.GetComponentInChildren<TextMeshProUGUI>().enabled = true;
+        }
     }
 
     private void OnDisable()
     {
         foreach (Slot slot in InventoryManager.Instance.inventorySlots)
+        {
+            Transform child = slot.transform;
+            if (child.GetComponent<Image>() != null) child.GetComponent<Image>().enabled = false;
+            if (child.GetComponentInChildren<TextMeshProUGUI>() != null) child.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+        }
+
+        foreach (TagSlot slot in InventoryManager.Instance.tagSlots)
         {
             Transform child = slot.transform;
             if (child.GetComponent<Image>() != null) child.GetComponent<Image>().enabled = false;
